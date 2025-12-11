9190e3bfa88fb24ec6b3b93d1ad0a1d5798033c3
johoon04 <your@email.com>
2025-12-12 08:02:06 +0900

    Fix: Add bcryptjs and update MongoDB options

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const { start } = require("repl");
 
